597f87558730af4ca088c9ac8a919fabfd5e5a8e
HVK2904 <Khang.HV210466@sis.hust.edu.vn>
2023-02-21 19:09:52 +0700

    add book_ticket function
    need to be check again

--- user-function.sql
+++ user-function.sql
@@ -115,3 +115,120 @@ END;
 $$
 
 -- How to filter bad input and still return some results?
+
+
+
+
+-- FUNCTION BOOK TICKET
+
+-- 1. Check valid user
+-- a. user_id
+CREATE OR REPLACE FUNCTION check_valid_user(userid int)
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	a int;
+BEGIN
+	select count(*) from customer into a where customer.user_id = userid;
+	if a = 0 then return false;
+	else return true;
+	end if;
+END
+$$;
+
+--b. email
+CREATE OR REPLACE FUNCTION check_valid_user(user_email text)
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	a int;
+BEGIN
+	select count(*) from customer into a where customer.email = user_email;
+	if a = 0 then return false;
+	else return true;
+	end if;
+END
+$$;
+
+--2. Check available_seat_in_screen
+
+CREATE OR REPLACE FUNCTION check_available_seat_in_screen(screenid int, seatid int)
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+	a int;
+	b int;
+	c int;
+BEGIN
+	select count(*) from screening into a where screen_id = screenid and available_seat != 0;
+	select count(*) from seat into c where seat_id = seatid;
+	if(a != 0 and c = 1) then
+	
+		select count(*) from ticket into b where screen_id = screenid and seat_id = seatid;
+		if( b = 0) then
+			return true;
+		else return false;
+		end if;
+	
+	else return false;
+	end if;
+END
+$$;
+
+
+--3. BOOK TICKET:
+
+--a. user_id
+
+
+CREATE OR REPLACE FUNCTION book_ticket(userid int, screenid int, seatid int)
+RETURNS void
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	a text;
+BEGIN
+	IF check_valid_user(userid) THEN
+		if check_available_seat_in_screen(screenid, seatid) then
+			select email from customer into a where user_id = userid;
+			INSERT INTO ticket values (default, a, 50, screenid, seatid, userid);
+			raise notice 'Success! Congratulations!';
+		else
+			raise notice 'The screening does not exist or the seatid was booked by someone else';
+			return;
+		end if;
+	ELSE
+		raise notice 'This user does not exist';
+		return;
+	END IF;
+END
+$$;
+
+
+--b. Email
+
+CREATE OR REPLACE FUNCTION book_ticket(u_email text, screenid int, seatid int)
+RETURNS void
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	a int;
+BEGIN
+	IF check_valid_user(u_email) THEN
+		if check_available_seat_in_screen(screenid, seatid) then
+			select user_id from customer into a where email = u_email;
+			INSERT INTO ticket values (default, u_email, 50, screenid, seatid, a);
+			raise notice 'Success! Congratulations!';
+		else
+			raise notice 'The screening does not exist or the seatid was booked by someone else';
+			return;
+		end if;
+	ELSE
+		raise notice 'This email does not exist';
+		return;
+	END IF;
+END
+$$;
